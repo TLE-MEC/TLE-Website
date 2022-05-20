a675a8ccff5a18405370a5e3d04d2c1ea80b243c
Hemanth R <hemanthrt18@gmail.com>
2022-05-20 22:28:56 +0530

    .

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,15 +10,24 @@ import about_ellipse_circle from '../../assets/svg/about_ellipse_circle.svg'
 function About() {
   return (
     <div className="about" id='about'>
+      <img src={about_ellipse_circle} className='about_ellipse_circle' alt="" />
       <div className='about__container'>
         <div className='about__top'>
-          <h1>ABOUT US</h1>
+          <h1 className='section__header'>ABOUT US</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis hendrerit proin mattis fusce. Adipiscing integer ornare id nisl elit, elit. Diam aliquam sit pellentesque odio venenatis nisl matti
           </p>
         </div>
         <div className='about__bottom'>
-
+          <div className='about_bottom_content'>
+            <div className='abc_top'>
+              <h3>What do we do ? </h3>
+            </div>
+            <div className='abc_bottom'>
+              <p>Data Structures and Algorithms Competitive Programming </p>
+            </div>
+          </div>
+          <img src={about_cubo} alt="" className="about_cubo" />  
         </div>
       </div>
     </div>
