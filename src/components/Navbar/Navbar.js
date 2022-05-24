510277421c6f368c9a76f1c9f6b4da790770e44f
Hemanth R <hemanthrt18@gmail.com>
2022-05-24 08:14:25 +0530

    nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import { useState, React } from 'react'
-import { HashLink } from 'react-router-hash-link';
+import { NavHashLink as NavLink } from 'react-router-hash-link';
 import { Drawer } from '@mui/material';
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { Link } from 'react-router-dom';
 
 import './Navbar.css';
 
 import logo from '../../assets/svg/logo.svg'
 import meclogo from '../../assets/svg/college-logo.svg'
-import { Link } from 'react-router-dom';
+
 
 
 function Navbar() {
@@ -28,21 +29,21 @@ function Navbar() {
         </Link>
 
         <div className='navbar_items'>
-          <HashLink to="#home" className='nav_item' smooth>
+          <NavLink to="#home" className='nav_item' smooth>
             Home
-          </HashLink>
-          <HashLink to="#about" className='nav_item' smooth>
+          </NavLink>
+          <NavLink to="#about" className='nav_item' smooth>
             About
-          </HashLink>
-          <HashLink to="#events" className='nav_item' smooth>
+          </NavLink>
+          <NavLink to="#events" className='nav_item' smooth>
             Events
-          </HashLink>
-          <HashLink to="#resources" className='nav_item' smooth>
+          </NavLink>
+          <NavLink to="#resources" className='nav_item' smooth>
             Resources
-          </HashLink>
-          <HashLink to="#team" className='nav_item' smooth>
+          </NavLink>
+          <NavLink to="#team" className='nav_item' smooth>
             Team
-          </HashLink>
+          </NavLink>
         </div>
         <a href='https://www.mec.ac.in/' target="_blank" rel="noreferrer">
           <img src={meclogo} alt="" className='meclogo' />
@@ -57,54 +58,56 @@ function Navbar() {
           <button className='nav_btn' onClick={handleDrawerOpen}>
             <HiOutlineMenuAlt3 className='hamburger'/>
           </button>
+          
         </div>
       </div>
       <Drawer
         open={open}
-        disableScrollLock={true}
+        disableScrollLock={false}
+        onClick={handleDrawerClose}
         onClose={(event, reason) => {
           if (reason !== 'backdropClick') {
               handleDrawerClose();
           } else if (reason !== 'escapeKeyDown') {
               handleDrawerClose();
           }
-      }}
+        }}
         
         anchor="left"
         PaperProps={{
           sx: {
             backgroundColor: "#FFFFFF",
-            boxShadow: "3px 0px 6px rgba(0, 0, 0, 0.25)"
+            boxShadow: "3px 0px 6px rgba(0, 0, 0, 0.25)",
+            height: '100vh'
           }
         }}
         sx={{
           background: 'rgba(255, 255, 255, 0.1)',
           backdropFilter: 'blur(5px)'
         }}
-      
       >
         <div className='nav__drawer'>
 
           <div className='nav__drawer_header'>
-            <Link to='/'>
+            <Link to='/team'>
               <img src={logo} alt="" className='nav_logo' />
             </Link>
             <div className='navbar_items_mob'>
-              <HashLink to="#home" className='nav_item' smooth>
+              <NavLink to="#home" className='nav_item' smooth>
                 Home
-              </HashLink>
-              <HashLink to="#about" className='nav_item' smooth>
+              </NavLink>
+              <NavLink to="#about" className='nav_item' smooth>
                 About
-              </HashLink>
-              <HashLink to="#events" className='nav_item' smooth>
+              </NavLink>
+              <NavLink to="#events" className='nav_item' smooth>
                 Events
-              </HashLink>
-              <HashLink to="#resources" className='nav_item' smooth>
+              </NavLink>
+              <NavLink to="#resources" className='nav_item' smooth>
                 Resources
-              </HashLink>
-              <HashLink to="#team" className='nav_item' smooth>
+              </NavLink>
+              <NavLink to="#team" className='nav_item' smooth>
                 Team
-              </HashLink>
+              </NavLink>
             </div>
           </div>
           <a href='https://www.mec.ac.in/' target="_blank" rel="noreferrer">
