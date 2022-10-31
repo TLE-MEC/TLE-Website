b94c0e47218ca9181c271c2c1bf4b1973e5a1c73
jaison080 <jaisondennis080@gmail.com>
2022-10-31 20:32:18 +0530

    Added Team Memebrs Page and Event also Updated

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -1,6 +1,9 @@
+import helloworld2 from '../assets/png/helloworld2.0.jpeg';
+import helloworld2card from '../assets/png/helloworld2.0card.jpeg';
 const eventData = [
   {
     id: 1,
+    isUpcoming:false,
     name: 'CallByValue',
     desc: 'An Instagram IGTV Series consisting of xMECians and their experience with DSA & CP released on Instagram. The speakers for this series included Aswin M Prabhu from Linkedin, Farha Kareem from Amazon, Prince Mathew from DE Shaw and Srividya Krishnakumar from Google.',
     image: 'https://res.cloudinary.com/dpt7fy6g5/image/upload/v1660110628/TLE%20Website/Events/CallByValue_e0nubs.png',
@@ -13,6 +16,7 @@ const eventData = [
   },
   {
     id: 2,
+    isUpcoming:false,
     name: 'Hacktoberfest Weekend',
     desc: 'TLE MEC collaborated with FOSSMEC and MACS to organize a workshop on Git & Github. A Github repository was created to encourage open source contributions while learning DSA and this repository had a total of 171 stars, 273 forks, 650 merged pull requests and 236 contributors.',
     image: 'https://res.cloudinary.com/dpt7fy6g5/image/upload/v1660110629/TLE%20Website/Events/hacktoberfest_rytjer.png',
@@ -25,6 +29,7 @@ const eventData = [
   },
   {
     id: 3,
+    isUpcoming:false,
     name: 'Hello World ',
     desc: 'The first official and inaugural session of TLE MEC, Hello World was conducted to introduce students to CP and DSA by going over their merits as well as walking them through some basic problems.',
     image: 'https://res.cloudinary.com/dpt7fy6g5/image/upload/v1660110630/TLE%20Website/Events/Hello_World_jhubgh.png',
@@ -37,6 +42,7 @@ const eventData = [
   },
   {
     id: 4,
+    isUpcoming:false,
     name: 'CodeSprint Contests',
     desc: 'A series of coding contests conducted in collaboration with MACS, with two tracks for participants to choose from - beginner and intermediate, conducted on the platform Hackerearth. The preliminary contests were one hour long and the advanced contests were 2.5 hours long.',
     image: 'https://res.cloudinary.com/dpt7fy6g5/image/upload/v1660110629/TLE%20Website/Events/Codesprint_d9lyao.png',
@@ -82,6 +88,7 @@ const eventData = [
   },
   {
     id: 5,
+    isUpcoming:false,
     name: 'DS404 Sessions',
     desc: 'TLE MEC collaborated with the Training Cell to organize a series of four events covering the basic data structures and algorithm topics. The sessions were on Algorithm Analysis, Arrays, Stacks & Queues.',
     image: 'https://res.cloudinary.com/dpt7fy6g5/image/upload/v1660110629/TLE%20Website/Events/DS404_cfr1mh.png',
@@ -94,6 +101,7 @@ const eventData = [
   },
   {
     id: 6,
+    isUpcoming:false,
     name: 'O(zone)',
     desc: 'O(zone) was a pair coding contest held by TLE MEC in collaboration with Github. The winners were awarded T-shirts sponsored by Github. ',
     image: 'https://res.cloudinary.com/dpt7fy6g5/image/upload/v1660110629/TLE%20Website/Events/Ozone_cqgjre.png',
@@ -121,6 +129,7 @@ const eventData = [
   },
   {
     id: 7,
+    isUpcoming:false,
     name: 'Code Conundrum',
     desc: 'Code Conundrum was a 2 hour-long programming contest, with questions ranging all difficulty levels and covering all major topics of DSA',
     image: 'https://res.cloudinary.com/dpt7fy6g5/image/upload/v1660110629/TLE%20Website/Events/CodeConundrum_wzn3cn.png',
@@ -133,6 +142,7 @@ const eventData = [
   },
   {
     id: 8,
+    isUpcoming:false,
     name: 'Global Pointers',
     desc: "When it comes to gathering tips on how to prepare for and present yourself in interviews, who better to ask than those who've been there and done that? For the first session of Global Pointers, TLE MEC brings to you, Srividya Krishnakumar, a Software Engineer at Google and an xMECian from the Batch of 2021.",
     image: 'https://res.cloudinary.com/dpt7fy6g5/image/upload/v1660110629/TLE%20Website/Events/GlobalPointers_mx1o7w.png',
@@ -142,6 +152,20 @@ const eventData = [
     github: '',
     participants: 100,
     leaderboard: []
+  },
+  {
+    id: 9,
+    name: "Hello World 2.0",
+    isUpcoming:true,
+    desc: "TLE MEC is all set to introduce you to DSA and CP and equip you for placements, job opportunities and upskilling through HELLO WORLD 2.0. Be with us on 2nd of November to kickstart the journey. And yesss, we do have a platform to show-off you and your efforts into the real WORLD!",
+    image:
+      helloworld2card,
+    dialog_img:
+      helloworld2,
+    date: "2nd Nov 2022",
+    youtube: "",
+    github: "",
+    leaderboard: [],
   },
 ];
 
