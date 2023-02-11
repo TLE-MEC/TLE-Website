f1297355609cca50b35fffff0688836e196574d8
ashleyth0mas <ashmariam@hotmail.com>
2023-02-11 22:59:00 +0530

    bloglist added

---
+++ src/pages/BlogListPage/BlogListPage.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import './BlogListPage.css'
+import blogPage_ellipse1 from '../../assets/svg/ellipse1.svg'
+import blogPage_ellipse2 from "../../assets/svg/ellipse1.svg";
+import blogPage_circle from "../../assets/svg/landing_circle.svg";
+import pic1 from "../../assets/png/pic1.jpg";
+import { BsClock } from "react-icons/bs";
+import { FaPenNib } from "react-icons/fa";
+import { SlEye } from "react-icons/sl";
+import { BiLike } from "react-icons/bi";
+import { Link } from 'react-router-dom';
+const BlogListPage = () => {
+  return (
+    <div id="container" >
+      <img src={blogPage_ellipse1} className="blogPage_first_ellipse" alt="" />
+      <img src={blogPage_circle} alt="" className="blogPage_circle" />
+      <img src={blogPage_ellipse2} alt="" className="blogPage_second_ellipse" />
+      <h1 className='heading'  >BLOGS</h1>
+        <div className='card'>
+          <div className='img'>
+            <img className='pic' src={pic1} alt=""></img>
+          </div>
+          <div className='content' >
+            <div className='dsa_icons'>
+              <div className='dsa'> DSA  |  programming  |  technology </div>
+              <div className='icons'>
+                <div className='icon'><BsClock /> 2 min read</div>
+                <div className='icon'><FaPenNib /> 208 words </div>
+                <div className='icon'><SlEye /> 15 Views</div>
+                <div className='icon'><BiLike />25 Likes</div>
+              </div>
+            </div>
+            <p className='title'>Title</p>
+            <div className='author_date'>
+              <div className='author'>By Author name </div>
+              <div> 20 May 2022</div>
+            </div>
+            <div className='content_info'>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis hendrerit proin mattis fusce. Adipiscing integer ornare id nisl elit, elit. Diam aliquam sit pellentesque odio venenatis nisl matti
+                <Link to="/blogpage" className='readmore' > read more....</Link></p>
+            </div>
+          </div>
+        </div>
+               <div className='card'>
+            <div className='img'>
+              <img className='pic' src={pic1} alt=""></img>
+            </div>
+            <div className='content' >
+              <div className='dsa_icons'>
+                <div className='dsa' > DSA  |  programming  |  technology </div>
+                <div className='icons' >
+                  <div className='icon'><BsClock /> 2 min read</div>
+                  <div className='icon'><FaPenNib /> 208 words </div>
+                  <div className='icon'><SlEye /> 15 Views</div>
+                  <div className='icon'><BiLike />25 Likes</div>
+                </div>
+              </div>
+              <p className='title'>Title</p>
+              <div className='author_date'>
+                <div className='author'>By Author name </div>
+                <div> 20 May 2022</div>
+              </div>
+              <div className='content_info'>
+               <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis hendrerit proin mattis fusce. Adipiscing integer ornare id nisl elit, elit. Diam aliquam sit pellentesque odio venenatis nisl matti
+                <Link to="/blogpage" className='readmore' > read more....</Link></p>
+              </div>
+            </div>
+                 </div>
+              <div className='card'>
+          <div className='img'>
+            <img className='pic' src={pic1} alt=""></img>
+          </div>
+          <div className='content' >
+            <div className='dsa_icons'>
+              <div className='dsa'> DSA  |  programming  |  technology </div>
+              <div className='icons'>
+                <div className='icon'><BsClock /> 2 min read</div>
+                <div className='icon'><FaPenNib /> 208 words </div>
+                <div className='icon'><SlEye /> 15 Views</div>
+                <div className='icon'><BiLike />25 Likes</div>
+              </div>
+            </div>
+            <p className='title'>Title</p>
+            <div className='author_date'>
+              <div className='author'>By Author name </div>
+              <div> 20 May 2022</div>
+            </div>
+            <div className='content_info'>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis hendrerit proin mattis fusce. Adipiscing integer ornare id nisl elit, elit. Diam aliquam sit pellentesque odio venenatis nisl matti
+                <Link to="/blogpage" className='readmore' > read more....</Link></p>
+                              </div>
+          </div>
+        </div>
+      </div>
+     )
+}
+export default BlogListPage
+        
+             
+               
