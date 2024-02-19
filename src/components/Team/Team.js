eca0a67301467b0f78bcc605f5d0723eaabe1c4c
Subru-37 <mdl21ec119@mec.ac.in>
2024-02-19 19:58:57 +0530

    changed team structure

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -12,12 +12,14 @@ import viewmore from "../../assets/svg/viewmore.svg";
 
 import hanna from '../../assets/team/2023-24/Hanna Salam.png';
 // import jeswin from '../../assets/team/2023-24/Jeswin Thomas.png';
-import sony from '../../assets/team/2023-24/Sony Thomas.png';
+// import sony from '../../assets/team/2023-24/Sony Thomas.png';
+import jeswin from '../../assets/team/2023-24/Jeswin Thomas.png'
+import arathy from '../../assets/team/2023-24/Arathy Sha.png'
 // import arathy from '../../assets/team/2023-24/Arathy Sha.png';
 // import vignesh from '../../assets/team/2023-24/Vignesh S Naik.png';
 import subru from '../../assets/team/2023-24/Subramani E.png';
 // import arpitha from '../../assets/team/2023-24/Arpitha Sudhir.png';
-import alaka from '../../assets/team/2023-24/Alaka AJ.png'
+// import alaka from '../../assets/team/2023-24/Alaka AJ.png'
 // import alex from '../../assets/team/2023-24/Alex Mathew Biju.png'
 // import adil from '../../assets/team/2023-24/Adil Naz Muhammed.png'
 // import neha from '../../assets/team/2023-24/Neha Mariam Mathew.png'
@@ -44,7 +46,7 @@ function Team() {
               data-aos="fade-up"
             />
             <span className="Team-chairpersonParent">
-              <img src={sony} className="hexagon" alt="" data-aos="fade-up" />
+              <img src={jeswin} className="hexagon" alt="" data-aos="fade-up" />
               {/* <span className="Teampage-overlay hexagon">Chairperson</span> */}
             </span>
           </div>
@@ -52,7 +54,7 @@ function Team() {
             <span className="Team-chairpersonParent">
               <img
                 className="hexagon"
-                src={alaka}
+                src={arathy}
                 alt=""
                 data-aos="fade-left"
               />
